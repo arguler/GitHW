bed53efccf71274151afc1241c05940b36bf8e62
arguler <arzaguler@gmail.com>
2023-04-18 07:41:58 +0300

    forth commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I will do it!");
+        System.out.println("Understanding the Git Exercises");
     }
+}

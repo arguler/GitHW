2dbc8ed9d8e4ba897905a97fba7dc9e60ce70ddd
arguler <arzaguler@gmail.com>
2023-04-20 08:28:02 +0300

    9. commit

--- src/Homework_03_Group/GroupHW_09.java
+++ src/Homework_03_Group/GroupHW_09.java
@@ -5,27 +5,26 @@ number in the array?*/
 public class GroupHW_09 {
     public static void main(String[] args) {
         int[][] array = {{15, 4, 58, 65},
-                {22, 14, 25, 89},
+                {22, 14, 25, 135},
                 {12, 45, 129, 36},
                 {11, 13, 34, 45}};
-        int[][] siralama = new int[1][16];
-        // int lowest = array[0][0];
-        int sira = array[0][0];
+        int largest = array[0][0];
+        int secondLargest = array[0][0];
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
-                if (array[i][j] > sira) {
-                    sira = array[i][j];
-                } else if (array[i][j] < sira) {
-                    i = i - 1;
-                    for (int k = 0; k < 16; k++) {
-                        siralama[0][k] = sira;
-                        System.out.println(siralama[0][k]);
-                    }
+                if (array[i][j] > largest) {
+                    largest = array[i][j];
                 }
             }
-
         }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] < largest && secondLargest < array[i][j]) {
+                    secondLargest = array[i][j];
+                }
+            }
+        }
+        System.out.println("Second largest number in array : " + secondLargest);
     }
-    //System.out.println("Minimum is  "+/*lowest*/  " and maximum is " + highest);
 }
-

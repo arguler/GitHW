5f6f482439e8c81bf1f46e261fe94dc164d216f4
arguler <arzaguler@gmail.com>
2023-05-12 23:28:40 +0300

    19. commit

--- src/Replit/repl_150.java
+++ src/Replit/repl_150.java
@@ -27,43 +27,26 @@ public class repl_150 {
         obj2.print("Frank", "Smith", 15, "tour");
     }
 }
-
 class Person {
     String name, lastName;
     int age;
 }
-
 class Employee extends Person {
-    int salary;
-
+       int salary;
     void print(String name, String lastName, int age, int salary) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
         this.salary = salary;
         System.out.println(name + " " + lastName + " " + age + " " + salary);
     }
 }
-
 class Student extends Employee {
     int grade;
-
     void print(String name, String lastName, int age, int grade) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-        this.grade = grade;
         System.out.println(name + " " + lastName + " " + age + " " + grade);
     }
 }
-
 class Retiree extends Student {
     String seniorActivity;
-
     void print(String name, String lastName, int age, String seniorActivity) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
         this.seniorActivity = seniorActivity;
         System.out.println(name + " " + lastName + " " + age + " " + seniorActivity);
     }
